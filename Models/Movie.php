89abdde2ff38b8d4a75f502c15a8c274e78e7f99
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-16 15:37:53 +0200

    create index.php & Movie.php

---
+++ Models/Movie.php
@@ -0,0 +1,14 @@
+<?php
+class Movie
+{
+    public $id;
+    public $title;
+    public $nationality;
+    public $date;
+    public $vote;
+    public $image;
+
+    public function __construct(){
+        
+    }
+}
