77a7d515ea8af858e5f6d7ebd1e7dea79e117bf4
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-16 17:10:48 +0200

    add $movies array

--- Models/Movie.php
+++ Models/Movie.php
@@ -3,12 +3,19 @@ class Movie
 {
     public $id;
     public $title;
+    public $original_title;
     public $nationality;
     public $date;
     public $vote;
     public $image;
 
-    public function __construct(){
-        
+    public function __construct($id, $title, $original_title, $nationality, $date, $vote, $image){
+        $this->id = $id;
+        $this->title = $title;
+        $this->original_title = $original_title;
+        $this->nationality = $nationality;
+        $this->date = $date;
+        $this->vote = $vote;
+        $this->image = $image;
     }
 }
